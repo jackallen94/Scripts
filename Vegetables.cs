244f408e6c122b47051799564261508258d3ed06
jackallen94 <k00212416@student.lit.ie>
2018-03-09 14:16:31 +0000

    Main Project Part 3
    In Part 2, I set the transform position on the y axis to -1 so we don't see it if it gets below 0 on the y axis and if isActive is set to false using a boolean we reuse the object that is not active, this will help optimisation and swapping of gameobjects

--- Vegetables.cs
+++ Vegetables.cs
@@ -15,13 +15,19 @@ public class Vegetables : MonoBehaviour {
 		IsActive = true; // setting the boolean isActive to true 
 		speed = xSpeed;
 		this.verticalVelocity = verticalVelocity; // this makes it less confusing setting one parameter to another.
-		transform.position = new Vector3(xStart,0,0);  
+		transform.position = new Vector3(xStart,0,0); // when we start Y is at 0  
 	}
 
 	private void Update() 
 	{
 		if (!IsActive)
-			return;
+			return; // update runs then
 		
-		verticalVelocity -= Gamegravity * Time.deltaTime; // update runs then
-}
+		verticalVelocity -= Gamegravity * Time.deltaTime; // using the const above allows us to set the gravity for 2.0 floats for all vegetables. 
+		transform.position += new Vector3(speed,verticalVelocity,0) * Time.deltaTime; // setting z to 0 and deltatime is the amount of time it takes to complete the last frame 
+	
+		if (transform.position.y < -1) // if the position of the veggie on the y axis is smaller than -1 then we don't see it
+			IsActive = false; // ready to reuse the object that is not active
+	
+	
+	}
