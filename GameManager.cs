a9a43746342d43efaab2bf724686283e9d6b7b5e
jackallen94 <k00212416@student.lit.ie>
2018-03-09 17:06:59 +0000

    Main Project Part 6
    Creating a GameManager script and managers game object in my Unity scene. Creating a list of vegetables and checking through the list to see which veggie is not active it will go back and grab a new one.

---
+++ GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	private List<Vegetables> veggies = new List<Vegetables> (); //instantiate our vegatables
+	public GameObject vegetablesPrefab; // grab the objects that are prefabs of our vegetables object (a prefab allows us to easily create duplicates that store all properties inside the prefab) 
+
+
+	private Vegetables GetVegetable() //  this will either get me something that is already isActive seen in our vegetables script or it will create a new instance
+	{
+
+		Vegetables v = veggies.Find (x => !x.IsActive); // this was quite difficult basically this looks through the list of veggies to find the first one that is not active, x being the veggie its looking at right now and also setting vegetable to variable v
+		// if the veggie is not active right now return the veggie to v field and stop executing. 
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
