b76e6f654cd528d033bd96ad54073f1dc9c0bd9b
jackallen94 <k00212416@student.lit.ie>
2018-03-27 17:45:21 +0100

    Main Project Part 31
    Setting and coding of sprite animations.
    
    creating an array of sprites adding each png into the array and remembering the time between each animation.

--- Vegetables.cs
+++ Vegetables.cs
@@ -9,6 +9,7 @@ public class Vegetables : MonoBehaviour {
 	public bool IsActive{set; get;} // if object is not active we can reuse it without having to create new veggie
 	private float verticalVelocity;
 	private float speed;
+	private SpriteRenderer sRenderer;
 
 	private bool isSliced = false;
 
@@ -17,6 +18,19 @@ public class Vegetables : MonoBehaviour {
 //		startVeggie (2.0f, 1, -1); // verticalVelocity of 2.0f, xspeed of 1 and xstart of -1
 //	}
 
+	public Sprite[] sprites;// creating an array of sprites
+	private int spriteIndex; // index position of the array
+	private float lastSpriteUpdate; // last frame sprite updated
+	private float spriteUpdateDelta = 0.2f;// time between each frame
+
+	private void Start()
+	{
+		sRenderer = GetComponent<SpriteRenderer>(); // getting the component sprite renderer
+
+
+
+	}
+
 	public void startVeggie(float verticalVelocity, float xSpeed, float xStart) // x axis speed and starting position
 	{
 		IsActive = true; // setting the boolean isActive to true 
@@ -24,6 +38,8 @@ public class Vegetables : MonoBehaviour {
 		this.verticalVelocity = verticalVelocity; // this makes it less confusing setting one parameter to another.
 		transform.position = new Vector3(xStart,0,0); // when we start Y is at 0  
 		isSliced =false; // resetting the veggie
+		spriteIndex = 0;
+		sRenderer.sprite = sprites[spriteIndex];
 	}
 
 	private void Update() 
@@ -34,6 +50,22 @@ public class Vegetables : MonoBehaviour {
 		verticalVelocity -= Gamegravity * Time.deltaTime; // using the const above allows us to set the gravity for 2.0 floats for all vegetables. 
 		transform.position += new Vector3(speed,verticalVelocity,0) * Time.deltaTime; // setting z to 0 and deltatime is the amount of time it takes to complete the last frame 
 	
+		if (isSliced) 
+		
+		{
+
+			if (Time.time - lastSpriteUpdate > spriteUpdateDelta) 
+			
+			{
+
+				lastSpriteUpdate = Time.time;
+				spriteIndex++;
+				sRenderer.sprite = sprites [spriteIndex];
+			}
+
+		}
+
+
 		if (transform.position.y < -1)
 		{// if the position of the veggie on the y axis is smaller than -1 then we don't see it
 			IsActive = false;
