95965b903995c2dadd4d4049eece7b337356db60
jackallen94 <k00212416@student.lit.ie>
2018-03-30 16:27:54 +0100

    Main Project Part 33
    Setting up a splash screen scene and using the scene manager to go to this scene.
    
    Also made sure that this scene was added to the build settings or error was occuring

---
+++ menuManScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; // managing scenes to go to another
+
+public class menuManScript : MonoBehaviour {
+
+	public void ToGame() // brings user to the game
+	{
+
+
+		SceneManager.LoadScene("Main");
+
+
+	}
+}
